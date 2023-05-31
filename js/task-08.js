a8101f77ab4d2c10c6b74b0db06be67a5973243d
Sheremeta Ivan <sheremetavanya2004@gmail.com>
2023-05-31 15:06:10 +0300

    finished-result-of-all-tasks

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();// сторінка не буде перезавантажуватись 
+
+  const email = loginForm.elements.email.value;
+  const password = loginForm.elements.password.value;
+
+  if (!email || !password) {
+    alert("Всі поля повинні бути заповнені!");
+  } else {
+    const formData = { email, password };
+    console.log(formData);
+    loginForm.reset();
+  }
+});
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,35 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const controlsDiv = document.querySelector('#controls');
+  const createBtn = controlsDiv.querySelector('[data-create]');
+  const destroyBtn = controlsDiv.querySelector('[data-destroy]');
+  const boxesDiv = document.querySelector('#boxes');
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, '0')}`;
+  }
+
+  function createBoxes(amount) {
+    for (let i = 0; i < amount; i+=1) {
+      const box = document.createElement('div');
+      const size = 30 + i * 10;
+
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+
+      boxesDiv.appendChild(box);
+    }
+  }
+
+  destroyBtn.addEventListener('click', destroyBoxes);
+  function destroyBoxes() {
+    boxesDiv.innerHTML = '';
+  }
+
+  createBtn.addEventListener('click', () => {
+    const amount = Number(document.querySelector('input').value);
+    createBoxes(amount);
+  });
+
+ 
